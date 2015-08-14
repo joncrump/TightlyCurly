1f07983ac58fde373f827b0b3200371325b5d285
Jon Crump <j.crump@outlook.com>
2015-08-13 18:20:51 -0700

    Added contentItem mappings.

--- contentItem.js
+++ contentItem.js
@@ -1,5 +1,69 @@
 // JavaScript source code
 [
-{
-    
-}]
+    {
+        "type":"TightlyCurly.Com.Common.Models.IAddress, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.Addresses",
+        "countProperty":"Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "ContentItemId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "MetaDescription",
+            "fieldType": "nvarchar",
+            "field": "MetaDescription",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "MetaKeywords",
+            "fieldType": "nvarchar",
+            "field": "MetaKeywords",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "Title",
+            "fieldType": "nvarchar",
+            "field": "Title"
+        },
+        {
+            "propertyName": "Description",
+            "fieldType": "nvarchar",
+            "field": "Description",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "IsActive",
+            "fieldType": "bit",
+            "field": "IsActive",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "Uri",
+            "fieldType": "nvarchar",
+            "field": "Uri"
+        },
+        {
+            "propertyName": "Name",
+            "fieldType": "nvarchar",
+            "field": "Name"
+        },
+        {
+            "propertyName": "Articles",
+            "lazyLoader": "GetArticlesByContentItem",
+            "joinMapping":
+                {
+                    "joinType": "left",
+                    "leftKey": "Id",
+                    "rightKey": "ContentItemId"
+                }
+        }]
+    }
+]
