e41350944345dc4d41e1611035ee101d463527da
Jon Crump <j.crump@outlook.com>
2015-08-13 18:43:50 -0700

    Added country mappings.

---
+++ country.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+[
+    {
+        "type":"TightlyCurly.Com.Common.Models.ICountry, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.Countries",
+        "countProperty":"Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "CountryIdId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "UpdatedDate",
+            "fieldType": "smalldatetime",
+            "field": "UpdatedDate"
+        },
+        {
+            "propertyName": "Name",
+            "fieldType": "nvarchar",
+            "field": "Name"
+        }]
+    }
+]
