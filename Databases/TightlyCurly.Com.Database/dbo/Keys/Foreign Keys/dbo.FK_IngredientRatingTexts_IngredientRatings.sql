8a5e868b6afe3a43f3e17f842c3fd0da15b48b46
Jon Crump <j.crump@outlook.com>
2015-08-17 16:38:03 -0700

    Added missing field and key.

---
+++ Databases/TightlyCurly.Com.Database/dbo/Keys/Foreign Keys/dbo.FK_IngredientRatingTexts_IngredientRatings.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[IngredientRatingTexts]
+	ADD CONSTRAINT [FK_IngredientRatingTexts_IngredientRatings]
+	FOREIGN KEY (IngredientRatingId)
+	REFERENCES [dbo].[IngredientRatings] (IngredientRatingId)
