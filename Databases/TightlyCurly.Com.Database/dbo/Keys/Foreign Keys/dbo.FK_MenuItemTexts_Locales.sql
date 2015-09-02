aa1977b19ca3c0d0e707e22c2422bd4ac23425d4
Jon Crump <j.crump@outlook.com>
2015-09-01 18:34:55 -0700

    Added menuItemTexts mappings.

---
+++ Databases/TightlyCurly.Com.Database/dbo/Keys/Foreign Keys/dbo.FK_MenuItemTexts_Locales.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE [dbo].[MenuItemTexts]
+	ADD CONSTRAINT [FK_MenuItemTexts_Locales]
+	FOREIGN KEY (LocaleId)
+	REFERENCES [dbo].[Locales] (LocaleId)
---
+++ menuItemText.js
@@ -0,0 +1,49 @@
+// JavaScript source code
+[
+    {
+        "type":"TightlyCurly.Com.Common.Models.IMenuItemText, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.MenuItemTexts",
+        "countProperty":"Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "MenuItemTextId",
+            "allowDbNull": false,
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "UpdatedDate",
+            "fieldType": "smalldatetime",
+            "field": "UpdatedDate"
+        },
+        {
+            "propertyName": "LocaleId",
+            "fieldType": "guid",
+            "field": "LocaleId"
+        },
+        {
+            "propertyName": "Text",
+            "fieldType": "nvarchar",
+            "field": "Text"
+        },
+        {
+            "propertyName": "Description",
+            "fieldType": "nvarchar",
+            "field": "Description",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "Locale",
+            "lazyLoader": "GetLocaleByMenuItemText",
+            "leftKey": "LocaleId",
+            "rightKey": "LocaleId",
+            "joinType": "left"
+        }]
+    }
+]
