8d2198b5f022e5a3d345b6d535dc9edbe6c9e7d2
Jon Crump <j.crump@outlook.com>
2015-09-14 18:48:45 -0700

    Added product mapping.

--- Libraries/Common/TightlyCurly.Com.Common.Models/IPersonRelationshipType.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TightlyCurly.Com.Common.Models
-{
-    public interface IPersonRelationshipType : IModel
-    {
-        string Name { get; set; }
-    }
-}
---
+++ product.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+[
+    {
+        "type": "TightlyCurly.Com.Common.Models.IProduct, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.Products",
+        "countProperty": "Id",
+        "propertyMappings:": [
+            {
+                "propertyName": "Id",
+                "fieldType": "guid",
+                "field": "ProductId",
+                "isPrimaryKey": true
+            },
+            {
+                "propertyName": "EnteredDate",
+                "fieldType": "smalldatetime",
+                "field": "EnteredDate"
+            },
+            {
+                "propertyName": "UpdatedDate",
+                "fieldType": "smalldatetime",
+                "field": "UpdatedDate"
+            }
+        ]
+    }
+]
