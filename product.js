202bc6248812a97001bae67e601c9701358a4107
Jon Crump <j.crump@outlook.com>
2015-09-15 18:40:23 -0700

    Added Product and Product Metadata mappings.  Tweaked gitignore.  Removed binary files.

--- product.js
+++ product.js
@@ -20,6 +20,26 @@
                 "propertyName": "UpdatedDate",
                 "fieldType": "smalldatetime",
                 "field": "UpdatedDate"
+            },
+            {
+                "propertyName": "ProductMetadata",
+                "lazyLoader": "GetProductMetaByProduct",
+                "joinMapping":
+                {
+                    "joinType": "left",
+                    "leftKey": "Id",
+                    "rightKey": "ProductId",
+                }
+            },
+            {
+                "propertyName": "Variants",
+                "lazyLoader": "GetVariantsByProduct",
+                "joinMapping":
+                {
+                    "joinType": "left",
+                    "leftKey": "Id",
+                    "rightKey": "ProductId",
+                }
             }
         ]
     }
---
+++ productMetadata.js
@@ -0,0 +1,77 @@
+// JavaScript source code
+[
+    {
+        "type": "TightlyCurly.Com.Common.Models.IProductMetadata, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.ProductMetadata",
+        "countProperty": "Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "ProductMetadataId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "ProductId",
+            "fieldType": "guid",
+            "field": "ProductId"
+        },
+        {
+            "propertyName": "Title",
+            "fieldType": "nvarchar",
+            "field": "Title"
+        },
+        {
+            "propertyName": "Product",
+            "lazyLoader": "GetProductByProductMetadata",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "ProductId",
+                "rightKey": "Id",
+            }
+        },
+        {
+            "propertyName": "Brands",
+            "lazyLoader": "GetBrandsByProductMetadata",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "Id",
+                "rightKey": "ProductMetadataId",
+                "joinTable": "dbo.ProductMetadata_Brands",
+                "joinTableJoinType": "left",
+                "joinTableLeftKey": "BrandId",
+                "joinTableRightKey": "BrandId"
+            }
+        },
+        {
+            "propertyName": "ProductLines",
+            "lazyLoader": "GetProductLinesByProductMetadata",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "Id",
+                "rightKey": "ProductMetadataId",
+                "joinTable": "dbo.ProductMetadata_ProductLines",
+                "joinTableJoinType": "left",
+                "joinTableLeftKey": "ProductLineId",
+                "joinTableRightKey": "ProductLineId"
+            }
+        },
+        {
+            "propertyName": "ImageUris",
+            "lazyLoader": "GetImageUrisByProductMetadata",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "Id",
+                "rightKey": "ProductMetadataId",
+                "joinTable": "dbo.ProductMetadata_ProductImageUris",
+                "joinTableJoinType": "left",
+                "joinTableLeftKey": "ProductImageUriId",
+                "joinTableRightKey": "ProductImageUriId"
+            }
+        }]
+    }
+]
