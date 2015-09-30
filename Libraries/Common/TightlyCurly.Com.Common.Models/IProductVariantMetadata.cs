20386e3df77899ad2747d5a69a835c5af076cdf2
Jon Crump <j.crump@outlook.com>
2015-09-29 18:48:34 -0700

    Added Product variant metadata mappings.

--- Libraries/Common/TightlyCurly.Com.Common.Models/IProductVariantMetadata.cs
+++ Libraries/Common/TightlyCurly.Com.Common.Models/IProductVariantMetadata.cs
@@ -1,9 +1,14 @@
-﻿namespace TightlyCurly.Com.Common.Models
+﻿using System;
+
+namespace TightlyCurly.Com.Common.Models
 {
     public interface IProductVariantMetadata : IModel
     {
         string Key { get; set; }
         string Value { get; set; }
+        Guid ProductVariantId { get; set; }
+        Guid EnteredById { get; set; }
         IUser EnteredBy { get; set; }
+        IProductVariant ProductVariant { get; set; }
     }
 }
--- productVariantMetadata.js
+++ productVariantMetadata.js
@@ -1,1 +1,64 @@
-// TODO: Add mappings
+[
+    {
+        "type":"TightlyCurly.Com.Common.Models.IProductVariantMetadata, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.ProductVariantMetadata",
+        "countProperty":"Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "ProductVariantMetadataId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "Key",
+            "fieldType": "nvarchar",
+            "field": "Key"
+        },
+        {
+            "propertyName": "Value",
+            "fieldType": "nvarchar",
+            "field": "Value"
+        },
+        {
+            "propertyName": "ProductVariantId",
+            "fieldType": "guid",
+            "field": "ProductVariantId"
+        },
+        {
+            "propertyName": "EnteredById",
+            "fieldType": "guid",
+            "field": "EnteredById"
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "UpdatedDate",
+            "fieldType": "guid",
+            "field": "UpdatedDate"
+        },
+        {
+             "propertyName": "EnteredBy",
+             "lazyLoader": "GetEnteredByProductVariantMetadata",
+             "joinMapping":
+             {
+                 "joinType": "left",
+                 "leftKey": "UserId",
+                 "rightKey": "UserId",
+             }
+        },
+        {
+            "propertyName": "ProductVariant",
+            "lazyLoader": "GetProductVariantByProductVariantMetadata",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "ProductVariantId",
+                "rightKey": "Id",
+            }
+        }]
+    }
+]
