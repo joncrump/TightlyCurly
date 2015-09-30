d391f961d5f741f13d471b2b70fc43c3b9633b03
Jon Crump <j.crump@outlook.com>
2015-09-29 18:52:51 -0700

    Added product type data mappings.

--- productType.js
+++ productType.js
@@ -1,1 +1,29 @@
-// TODO: Add mappings
+[
+    {
+        "type":"TightlyCurly.Com.Common.Models.IProductType, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.ProductTypes",
+        "countProperty":"Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "ProductTypeId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "Type",
+            "fieldType": "nvarchar",
+            "field": "Type"
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "UpdatedDate",
+            "fieldType": "smalldatetime",
+            "field": "UpdatedDate"
+        }]
+    }
+]
