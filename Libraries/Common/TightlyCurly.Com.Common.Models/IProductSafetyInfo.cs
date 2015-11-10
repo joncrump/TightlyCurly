17232706de07ec6d00042ada1a2f279f81deb455
Jon Crump <j.crump@outlook.com>
2015-11-09 18:30:03 -0800

    Added Product safety info mappings.

--- Libraries/Common/TightlyCurly.Com.Common.Models/IProductSafetyInfo.cs
+++ Libraries/Common/TightlyCurly.Com.Common.Models/IProductSafetyInfo.cs
@@ -5,7 +5,7 @@ namespace TightlyCurly.Com.Common.Models
 {
     public interface IProductSafetyInfo : IModel
     {
-        ProductSafetyInfoType ProductSafetyInfoType { get; set; }
+        string ProductSafetyInfoType { get; set; }
         string Text { get; set; }
         int? Order { get; set; }
         string Source { get; set; }
@@ -15,8 +15,7 @@ namespace TightlyCurly.Com.Common.Models
         string Notes { get; set; }
         bool? IsActive { get; set; }
         DateTime? EffectiveDate { get; set; }
-        DateTime StopDate { get; set; }
-        IEnumerable<IProductSafetyInfoComment> Comments { get; set; } 
+        DateTime? StopDate { get; set; }
         IProductVariant ProductVariant { get; set; }
         IUser EnteredBy { get; set; }
         ILocale Locale { get; set; }
--- productSafetyInfo.js
+++ productSafetyInfo.js
@@ -1,1 +1,121 @@
 // TODO: Add mappings
+[
+    {
+        "type": "TightlyCurly.Com.Common.Models.IProductSafetyInfo, TightlyCurly.Com.Common.Models",
+        "dataSource": "dbo.ProductSafetyInfos",
+        "countProperty": "Id",
+        "propertyMappings:": [
+        {
+            "propertyName": "Id",
+            "fieldType": "guid",
+            "field": "ProductSafetyInfoId",
+            "isPrimaryKey": true
+        },
+        {
+            "propertyName": "ProductSafetyInfoType",
+            "fieldType": "nvarchar",
+            "field": "ProductSafetyInfoType",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "Text",
+            "fieldType": "nvarchar",
+            "field": "Text"
+        },
+        {
+            "propertyName": "Order",
+            "fieldType": "int",
+            "field": "Order",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "Source",
+            "fieldType": "nvarchar",
+            "field": "Source",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "ProductVariantId",
+            "fieldType": "guid",
+            "field": "ProductVariantId"
+        },
+        {
+            "propertyName": "EnteredById",
+            "fieldType": "guid",
+            "field": "EnteredById"
+        },
+        {
+            "propertyName": "LocaleId",
+            "fieldType": "guid",
+            "field": "LocaleId"
+        },
+        {
+            "propertyName": "Notes",
+            "fieldType": "nvarchar",
+            "field": "Notes",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "IsActive",
+            "fieldType": "bool",
+            "field": "IsActive",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "EffectiveDate",
+            "fieldType": "smalldatetime",
+            "field": "EffectiveDate",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "StopDate",
+            "fieldType": "smalldatetime",
+            "field": "StopDate",
+            "allowDbNull": true
+        },
+        {
+            "propertyName": "EnteredDate",
+            "fieldType": "smalldatetime",
+            "field": "EnteredDate"
+        },
+        {
+            "propertyName": "UpdatedDate",
+            "fieldType": "smalldatetime",
+            "field": "UpdatedDate"
+        },
+        {
+             "propertyName": "ProductVariant",
+             "lazyLoader": "GetProductVariantByProductSafetyInfo",
+             "joinMapping":
+             {
+                 "joinType": "left",
+                 "leftKey": "Id",
+                 "rightKey": "ProductSafetyInfoId",
+                 "joinTable": "dbo.ProductSafetyInfos_ProductVariants",
+                 "joinTableJoinType": "left",
+                 "joinTableLeftKey": "ProductVariantId",
+                 "joinTableRightKey": "ProductVariantId"
+             }
+        },
+        {
+             "propertyName": "EnteredBy",
+             "lazyLoader": "GetEnteredByByProductSafetyInfo",
+             "joinMapping":
+             {
+                 "joinType": "left",
+                 "leftKey": "EnteredById",
+                 "rightKey": "Id",
+             }
+        },
+        {
+            "propertyName": "Locale",
+            "lazyLoader": "GetLocaleByProductSafetyInfo",
+            "joinMapping":
+            {
+                "joinType": "left",
+                "leftKey": "LocaleId",
+                "rightKey": "Id",
+            }
+        }]
+    }
+]
