4e8f1fca1c51844a5c8152f953b919cf96562d1e
Jon Crump <jon@tightlycurly.com>
2019-11-30 18:18:53 -0500

    Added null guard.

--- Libraries/Common/TightlyCurly.Com.Common.Data/ObjectMappingFactory.cs
+++ Libraries/Common/TightlyCurly.Com.Common.Data/ObjectMappingFactory.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TightlyCurly.Com.Common.Data.Mappings;
 
 namespace TightlyCurly.Com.Common.Data
 {
     public class ObjectMappingFactory : IObjectMappingFactory
     {
-        public IMapping GetMappingForType(Type objectType, MappingKind mappingKind = MappingKind.Attribute)
+        public IMapping GetMappingForType(Type objectType, MappingKind mappingKind)
         {
+            objectType.EnsureIsNotNull(nameof(objectType));
+
             throw new NotImplementedException();
         }
     }
